de7b01706f0f362e3b97c6c72967a910344ca743
bmariem <mariem.borchani@gmail.com>
2022-02-11 12:40:59 +0100

    📟 add pagination on Home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,7 @@
 // Lib
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 // Components
 import Spinner from "../../components/Spinner/Spinner";
@@ -15,29 +16,43 @@ const Home = () => {
   // STATES
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+
+  // Maximum number of offers per page
+  const limit = 5;
+
+  const handlePageClick = (event) => {
+    console.log(event);
+    setPage(event.selected + 1);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/offers"
+          `https://lereacteur-vinted-api.herokuapp.com/offers?page=${page}&limit=${limit}`
         );
 
-        setData(response.data);
+        // update data state with all the offers
+        setData(response.data.offers);
+
+        // Calculate the maximum number of pages
+        setPageCount(Math.ceil(Number(response.data.count) / limit));
         setIsLoading(false);
       } catch (error) {
         console.log(error.response);
       }
     };
     fetchData();
-  }, []);
+  }, [page]);
 
   return isLoading ? (
     <Spinner />
   ) : (
     <div className="Home">
       <div className="home-bg-img">
-        <img src={imgEffect} alt="forme" className="home-forme" />
+        <img src={imgEffect} alt="shape" className="home-img-shape" />
         <div>
           <div className="home-msg">
             Prêts à faire du tri dans vos placards ?
@@ -47,10 +62,21 @@ const Home = () => {
       </div>
 
       <div className="cards">
-        {/* Récuperer toutes offres */}
-        {data.offers.map((offer) => {
+        {/* Get all offers */}
+        {data.map((offer) => {
           return <Card offer={offer} key={offer._id} />;
         })}
+      </div>
+      <div className="home-pagination">
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+        />
       </div>
     </div>
   );
