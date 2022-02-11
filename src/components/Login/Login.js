1f41668dafe5b21eb71a5d4aa494c0dcb17bee99
bmariem <mariem.borchani@gmail.com>
2022-02-11 15:08:13 +0100

    Quick fix to Deployment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 // Lib
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 // Components
