f61947c8e316a2a82dc187a9d3100641651c8016
bmariem <mariem.borchani@gmail.com>
2022-02-15 19:24:59 +0100

    Test done with API

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://lereacteur-vinted-api.herokuapp.com/", // API Reacteur
-  baseURL: "https://api-vinted-project.herokuapp.com/", // mon API
+  baseURL: "https://lereacteur-vinted-api.herokuapp.com/", // API Reacteur
+  //baseURL: "https://api-vinted-project.herokuapp.com/", // mon API
 });
 
 export default instance;
